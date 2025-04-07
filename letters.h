020bfa496081975efccb0d3c5f7197cd50ab0408
MeeeeRP <102824059+MeeeeRP@users.noreply.github.com>
2025-04-07 04:27:44 -0400

    letters.h prototypes

---
+++ letters.h
@@ -0,0 +1,7 @@
+void make_heart(int (*)[8]);
+void make_B(int (*)[5]);
+void make_E(int (*)[5]);
+void make_M(int (*)[5]);
+void make_Y(int (*)[5]);
+void make_F(int (*)[5]);
+void make_question(int (*)[8]);
